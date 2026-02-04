9e66758387e189fa9f69cb1974532e769bac1f84
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-04 20:18:27 +0500

    new: Pygame setup

---
+++ main.py
@@ -0,0 +1,21 @@
+# Imports.
+import pygame, sys
+
+# Pygame setup.
+pygame.init()
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+# Game loop.
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("black")
+
+    pygame.display.update()
+    clock.tick(60)
