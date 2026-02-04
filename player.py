4213c6c8d8695b070f19c3763db93187caae82fa
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-04 21:24:23 +0500

    update: Player gravity

--- player.py
+++ player.py
@@ -10,8 +10,11 @@ class Player(pygame.sprite.Sprite):
         self.image.fill("cyan")
         self.rect = self.image.get_rect(topleft=pos)
 
+        # Movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_power = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -23,7 +26,12 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += int(self.direction.y)
+
     def update(self):
         self.get_input()
+        self.apply_gravity()
 
         self.rect.x += int(self.direction.x) * self.speed
