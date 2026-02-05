31872ac8e5d890e85e7c759b6276cb3baf715751
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-05 15:44:05 +0500

    new: cut graphic import helper

--- code/support.py
+++ code/support.py
@@ -3,6 +3,9 @@ from os import walk
 from csv import reader
 import pygame
 
+from settings import tile_size
+from game_data import cut_graphics
+
 
 # Use to import animation frames
 def import_folder(path):
@@ -26,3 +29,23 @@ def import_csv_data(path):
             terrain_map.append(list(row))
 
     return terrain_map
+
+
+# Use to get graphics in a single png file.
+def import_cut_graphics(type):
+    cut_tiles = []
+
+    surf = pygame.image.load(cut_graphics[type]).convert_alpha()
+    tile_num_x = int(surf.get_width() // tile_size)
+    tile_num_y = int(surf.get_height() // tile_size)
+
+    for row in range(tile_num_y):
+        for col in range(tile_num_x):
+            x = col * tile_size
+            y = row * tile_size
+
+            new_surf = pygame.Surface((tile_size, tile_size))
+            new_surf.blit(surf, (0, 0), pygame.Rect(x, y, tile_size, tile_size))
+            cut_tiles.append(new_surf)
+
+    return cut_tiles
