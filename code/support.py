5f55c1f1049899fd26d3b54d18a84b2d90b398da
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-05 14:46:29 +0500

    new: reading level data files into lists

--- code/support.py
+++ code/support.py
@@ -1,8 +1,10 @@
 # Imports
 from os import walk
+from csv import reader
 import pygame
 
 
+# Use to import animation frames
 def import_folder(path):
     surfaces = []
 
@@ -11,3 +13,16 @@ def import_folder(path):
             surfaces.append(pygame.image.load(path + "/" + filename).convert_alpha())
 
     return surfaces
+
+
+# Use to import tailed csv data.
+def import_csv_data(path):
+    terrain_map = []
+
+    with open(path) as map:
+        level = reader(map, delimiter=",")
+
+        for row in level:
+            terrain_map.append(list(row))
+
+    return terrain_map
