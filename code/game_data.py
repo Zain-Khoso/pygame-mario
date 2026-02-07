1e21fce9937fa4aa00234d9b11949978a879d2e6
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-07 10:54:31 +0500

    new: overworld levels data

--- code/game_data.py
+++ code/game_data.py
@@ -16,14 +16,30 @@ animation_frames = {
     "enemies": "./graphics/enemy/run",
 }
 
-level_0 = {
-    "bg palms": "./levels/0/level_0_bg_palms.csv",
-    "coins": "./levels/0/level_0_coins.csv",
-    "constraints": "./levels/0/level_0_constraints.csv",
-    "crates": "./levels/0/level_0_crates.csv",
-    "enemies": "./levels/0/level_0_enemies.csv",
-    "fg palms": "./levels/0/level_0_fg_palms.csv",
-    "grass": "./levels/0/level_0_grass.csv",
-    "player": "./levels/0/level_0_player.csv",
-    "terrain": "./levels/0/level_0_terrain.csv",
+# level_0 = {
+#     "bg palms": "./levels/0/level_0_bg_palms.csv",
+#     "coins": "./levels/0/level_0_coins.csv",
+#     "constraints": "./levels/0/level_0_constraints.csv",
+#     "crates": "./levels/0/level_0_crates.csv",
+#     "enemies": "./levels/0/level_0_enemies.csv",
+#     "fg palms": "./levels/0/level_0_fg_palms.csv",
+#     "grass": "./levels/0/level_0_grass.csv",
+#     "player": "./levels/0/level_0_player.csv",
+#     "terrain": "./levels/0/level_0_terrain.csv",
+# }
+
+level_0 = {"node_pos": (110, 400), "content": "this is level 0", "unlock": 1}
+level_1 = {"node_pos": (300, 220), "content": "this is level 1", "unlock": 2}
+level_2 = {"node_pos": (480, 610), "content": "this is level 3", "unlock": 3}
+level_3 = {"node_pos": (610, 350), "content": "this is level 4", "unlock": 4}
+level_4 = {"node_pos": (880, 210), "content": "this is level 5", "unlock": 5}
+level_5 = {"node_pos": (1050, 400), "content": "this is level 5", "unlock": 5}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5,
 }
