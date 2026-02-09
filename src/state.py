016ea5adf8831b62703a03a0e9b92a663a3ba329
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-09 15:26:09 +0500

    update: programatically creating a state.csv file

--- src/state.py
+++ src/state.py
@@ -1,5 +1,5 @@
 # Lib Imports
-import csv, json
+import sys, pygame, csv, json
 
 # Local Imports
 from .settings import csv_state
@@ -32,16 +32,41 @@ class State:
         return json.dumps(state)
 
     def load(self):
-        with open(csv_state, "r") as raw:
-            file = csv.DictReader(raw)
-            state = next(file)
+        try:
+            with open(csv_state, "r") as raw:
+                file = csv.DictReader(raw)
+                state = next(file)
 
-            self.unlocked_levels = int(state["unlocked_levels"])
-            self.current_level = int(state["current_level"])
-            self.max_health = int(state["max_health"])
-            self.current_health = int(state["current_health"])
-            self.total_coins = int(state["total_coins"])
-            self.current_coins = int(state["current_coins"])
+                self.unlocked_levels = int(state["unlocked_levels"])
+                self.current_level = int(state["current_level"])
+                self.max_health = int(state["max_health"])
+                self.current_health = int(state["current_health"])
+                self.total_coins = int(state["total_coins"])
+                self.current_coins = int(state["current_coins"])
+        except FileNotFoundError:
+            self.make_state_file()
+            self.load()
+
+    def make_state_file(self):
+        state = {
+            "unlocked_levels": 0,
+            "current_level": 0,
+            "max_health": 100,
+            "current_health": 100,
+            "total_coins": 0,
+            "current_coins": 0,
+        }
+
+        try:
+            with open(csv_state, "w") as raw:
+                file = csv.DictWriter(raw, fieldnames=state.keys())
+
+                file.writeheader()
+                file.writerow(state)
+        except:
+            print("\nUnable to find or create a game state.\n")
+            pygame.quit()
+            sys.exit()
 
     def save(self):
         state = json.loads(str(self))
